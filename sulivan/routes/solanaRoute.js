c8021666ca79ce999b91dd0a9a01bf584cbd3c35
Suigiro <luizmilenium@hotmail.com>
2024-08-06 15:07:42 -0300

    :sparkles: feat: Integrate Solana Contracts V4

--- sulivan/routes/solanaRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {balance, sendTransaction} = required('../controller/solanaContracts');
-
-router.route('/balance/:address').get(balance);
-router.route('/send-transaction').post(sendTransaction);
-
-
-module.exports = router;
